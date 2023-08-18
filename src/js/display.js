5d464c41b97672acd7404fea575038618f316270
Jeremy Felt <jeremy.felt@gmail.com>
2023-08-18 09:44:29 -0700

    Add production changes

--- src/js/display.js
+++ src/js/display.js
@@ -1,11 +1,37 @@
-(function( $ ) {
-    $.getJSON( "../../data/last-observation.json", function( data ) {
-        let seconds = Date.parse( data[0].DateObserved + " " + data[0].HourObserved + ":00:00 " + data[0].LocalTimeZone );
-        let date = new Date( seconds );
+{
+	fetch( '../../data/last-observation.json')
+	.then( response => {
+		return response.json();
+	} )
+	.then( data => {
 
-        $( ".my-aqi" ).text( data[0].AQI );
-        $( ".my-category" ).text( data[0].Category.Name );
-        $( ".my-location" ).text( data[0].ReportingArea + ", " + data[0].StateCode );
-        $( ".my-time" ).text( date );
-    } );
-} ( jQuery ) );
+		const [
+			year,
+			month,
+			day
+		] = data[0].DateObserved.split("-");
+		const hour = data[0].HourObserved;
+		const dateObj = new Date( year, month - 1, day, hour, 0, 0 );
+
+		const options = {
+			weekday: 'short',
+			month: 'short',
+			day: 'numeric',
+			year: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			timeZoneName: 'long'
+		};
+
+		const formattedDate = dateObj.toLocaleString('en-US', options);
+
+		document.querySelector( '.my-aqi' ).innerText = data[0].AQI;
+		document.querySelector( '.my-category' ).innerText = data[0].Category.Name;
+		document.querySelector( '.my-location' ).innerText = data[0].ReportingArea + ", " + data[0].StateCode;
+		document.querySelector( '.my-time' ).innerText = formattedDate;
+	})
+	.catch( error => {
+		console.log( error );
+	});
+}
